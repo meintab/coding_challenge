9f3a1f3d7374e149447b3d1c2fe28453f62ee26b
Alamein Tabunda <alameinptabunda@su.edu.ph>
2023-08-04 01:48:13 +0800

    Needs UI

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $products =  Product::all();
+        return Inertia::render(
+            'Products/Index',
+            [
+                'products' => $products
+            ]
+            );
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return Inertia::render('Products/Create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string'
+        ]);
+
+        Product::create([
+            'name' => $request->name 
+        ]);
+
+        return redirect()->route('producst.index')->with('message', 'Product Added');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Product $product)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Product $product)
+    {
+        return Inertia::render(
+            'Products/Edit',
+            [
+                'product' => $product
+            ]
+            );
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Product $product)
+    {
+        $request->validate([
+            'name' => 'required|string'
+        ]);
+
+        $product->name = $request->name;
+        return redirect()->route('producst.index')->with('message', 'Product Updated');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Product $product)
+    {
+        $product->delete();
+        sleep(1);
+        return redirect()->route('producst.index')->with('message', 'Product Deleted');
+    }
+}
---
+++ resources/js/Pages/Products/Index.vue
@@ -0,0 +1,68 @@
+<script setup>
+import BreezeAuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue'
+import {Head,Link,useForm} from "@inertiajs/vue3"
+import BreezeButton from "@/Components/PrimaryButton.vue"
+import {Inertia} from "@inertiajs/vue3" 
+
+const props = defineProps({
+    products: {
+        type: Object,
+        default: () => ({})
+    }
+});
+
+const form = useForm();
+
+
+
+</script>
+
+<template>
+    <Head title="Products" />
+    <BreezeAuthenticatedLayout>
+        <template #header>
+            <h2 class="text-xl font-semibold text-gray-800">
+                Products
+            </h2>
+        </template>
+        <div class="py-12">
+            <div class="mx-auto">
+                <div 
+                v-if="$page.props.flash.message"
+                class="p-4 mb-4 text-sm text-green-700 rounded-lg"
+                role="alert"
+                >
+                    <span class="font-medium">
+                        {{ $page.props.flash.message }}
+                    </span>
+                </div>
+                <div class="overflow-hiddin bg-white shadow-sm">
+                    <div class="p-6 bg-white border-b border-gray-200">
+                        <div class="mb-2">
+                            <Link :href="route('products.create')">
+                                <BreezeButton>Add Blog</BreezeButton>
+                            </Link>
+                        </div>
+                    </div>
+                    <div>
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>Name</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr
+                                v-for="product in products"
+                                :key="product.id">
+                                <td>{{product.name }}</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </BreezeAuthenticatedLayout>
+</template>
